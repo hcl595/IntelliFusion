9bc482922e74473c81c89834e2bf8e49a6e2d0c3
Suggestion <84503411+hcl595@users.noreply.github.com>
2023-07-18 17:32:04 +0800

    Support Wigets,Fixed Bugs

---
+++ src/views/data.py
@@ -0,0 +1,76 @@
+# data.py | Realizer Version 0.1.7(202307182000) Developer Alpha
+from pathlib import Path
+from loguru import logger
+from sqlalchemy import Column, Index, Integer, String, UniqueConstraint, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 基础类
+APP_DIR = Path(__file__).parent
+DATA_DIR = APP_DIR / "data"
+DATABASE_FILE = DATA_DIR / "models.sqlite"
+LOG_FILE = DATA_DIR / "models.log"
+
+Base = declarative_base()
+engine = create_engine(f"sqlite:///{DATABASE_FILE}", echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class userInfo(Base):
+    # 数据库中存储的表名
+    __tablename__ = "userInfo"
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="主键")
+    account = Column(String(32), index=True, nullable=False, comment="姓名")
+    password = Column(String(32), nullable=False, comment="密码")
+    mail = Column(String(32), index=True, nullable=True, comment="邮箱")
+    __table__args__ = (
+        UniqueConstraint("id", "name"),  # 联合唯一约束
+        Index("name", unique=True),       # 联合唯一索引
+    )
+
+
+
+    def __str__(self):
+        return f"object : <id:{self.id} name:{self.name} key:{self.key}>"
+
+
+class models(Base):
+    __tablename__ = "models"
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="主键")
+    type = Column(String(32), nullable=False, comment="类型")
+    name = Column(String(32), nullable=False, comment="模型")
+    url = Column(String(32), nullable=False, comment="地址")
+    APIkey = Column(Integer, nullable=False, comment="密钥")
+    LaunchCompiler = Column(String(32), nullable=True, comment="启动编译器")
+    LaunchUrl = Column(String(32), nullable=True, comment="启动地址")
+    Display = Column(String(32), nullable=True, comment="显示")
+    __table__args__ = (
+        UniqueConstraint("id", "url"),  # 联合唯一约束
+        Index("url", unique=True),       # 联合唯一索引
+    )
+
+    def __str__(self):
+        return f"object : <id:{self.id} url:{self.url}>"
+
+
+def SetupDatabase():
+    logger.info("models.sqlite does not exist and is being created automatically...")
+    Base.metadata.create_all(engine)
+    user_instance = userInfo(
+        account="admin",
+        password="admin1234",
+    )
+    session.add(user_instance)
+    BasisModel = models(
+        type="OpenAI",
+        name="text-davinci-002",
+        url="https:\\\\ai.fakeopen.com\\v1",
+        APIkey="None",
+        LaunchCompiler="NONE",
+        LaunchUrl="NONE",
+    )
+    session.add(BasisModel)
+    session.commit()
+    logger.info("models.sqlite is created successfully!")
+
---
+++ src/views/setup.py
@@ -0,0 +1,39 @@
+# config.py | Realizer Version 0.1.7(202307182000) Developer Alpha
+from pathlib import Path
+from loguru import logger
+import json
+from data import SetupDatabase
+
+APP_DIR = Path(__file__).parent
+DATA_DIR = APP_DIR / "data"
+CONFIG_FILE = DATA_DIR / "config.json"
+DATABASE_FILE = DATA_DIR / "models.sqlite"
+LOG_FILE = DATA_DIR / "models.log"
+
+def setup():
+        if not DATA_DIR.exists():
+            DATA_DIR.mkdir()
+        if not LOG_FILE.exists():
+            logger.add(LOG_FILE)
+            logger.info('models.log is created successfully')
+        if not DATABASE_FILE.exists():
+            SetupDatabase()
+        if not CONFIG_FILE.exists():
+            logger.info("config.json doesn't exist")
+            logger.info("create config.json")
+            data = {
+                "BaseConfig": {"devmode": False, "debug": False, "keeplogin": True, "TimeOut": 60},
+                "RemoteConfig": {"host": "127.0.0.1", "port": "5000"},
+                "ModelConfig": {
+                    "DefaultModel": "text-davinci-002",
+                    "SecondModel": None,
+                    "ThirdModel": None,
+                },
+            }
+            with CONFIG_FILE.open("w", encoding="utf-8") as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
+            logger.info("config.json is created successfully")
+
+
+if __name__ == '__main__':
+     setup()
