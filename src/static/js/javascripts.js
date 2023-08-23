11e213b7cec363295b630d610c828256071c8c27
Suggestion <84503411+hcl595@users.noreply.github.com>
2023-08-23 15:55:27 +0800

    fixed bugs

--- src/static/js/javascripts.js
+++ src/static/js/javascripts.js
@@ -490,7 +490,7 @@ function Refresh_Tabs(){
                             </div>\
                             <input id="model-input-'+ data[i].id +'" type="hidden" value='+ data[i].name +' />\
                             <div class="button-area">\
-                                <button type="submit" id="SendInput" value="'+ data[i].id +'" onclick="SendInput(`'+ data[i].id +'`)">发 送</button>\
+                                <button type="submit" id="SendInput" value="'+ data[i].id +'" onclick="SendInput(`'+ data[i].id +'`)"><i class="fa fa-send"></i></button>\
                             </div>\
                         </div>\
                     </div>')
@@ -519,7 +519,7 @@ function Refresh_Tabs(){
                                 </div>\
                                 <input id="model-input-'+ data[i].id +'" type="hidden" value='+ data[i].name +' />\
                                 <div class="button-area">\
-                                <button type="submit" id="SendInput" value="'+ data[i].id +'" onclick="SendInput(`'+ data[i].id +'`)">发 送</button>\
+                                <button type="submit" id="SendInput" value="'+ data[i].id +'" onclick="SendInput(`'+ data[i].id +'`)"><i class="fa fa-send"></i></button>\
                                 </div>\
                             </div>\
                         </div>')
